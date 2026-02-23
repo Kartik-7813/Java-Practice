b09293c289bb2260b88a78c37cff5eb41db148a4
Kartik Chinchole <kartikchinchole47@gmail.com>
2026-02-23 23:19:27 +0530

    Super keyword in Java

---
+++ Super_Keyword/Vehicle.java
@@ -0,0 +1,17 @@
+package Super_Keyword;
+
+public class Vehicle {
+
+    public int minSpeed = 7;
+    public int maxSpeed = 120;
+
+}
+
+class Person {
+
+    Person() {
+
+        System.out.println("Person class constructor");
+    }
+
+}
